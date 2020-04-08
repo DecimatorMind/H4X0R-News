05a06e244e235d3036f2c7c6c5cf1aae9d4c1b50
Pranjal Bhardwaj <E19CSE432@bennett.edu.in>
2020-04-09 01:23:55 +0530

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Pranjal Bhardwaj on 09/04/20.
+//  Copyright © 2020 Pranjal Bhardwaj. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
